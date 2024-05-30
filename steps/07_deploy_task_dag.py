51bd119a7d0cd169a58ed7c5e039fa92d1618c9e
Christen Marshall <108291161+sfc-gh-chmarshall@users.noreply.github.com>
2024-05-30 16:05:24 +0000

    Forgot a warehouse definition

--- steps/07_deploy_task_dag.py
+++ steps/07_deploy_task_dag.py
@@ -28,7 +28,7 @@ def main(session: Session) -> str:
 
     # Define the DAG
     dag_name = "HOL_DAG"
-    dag = DAG(dag_name, schedule=timedelta(days=1))
+    dag = DAG(dag_name, schedule=timedelta(days=1), warehouse=warehouse_name)
     with dag:
         dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')", warehouse=warehouse_name)
         dag_task2 = DAGTask("LOAD_DAILY_CITY_METRICS_TASK", definition="CALL LOAD_DAILY_CITY_METRICS_SP()", warehouse=warehouse_name)
