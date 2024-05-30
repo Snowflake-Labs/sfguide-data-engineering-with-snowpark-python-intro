96a85f3c3c85cc4ee4035df2a422967ca30c38ee
Christen Marshall <108291161+sfc-gh-chmarshall@users.noreply.github.com>
2024-05-30 15:24:19 +0000

    Swapping back to regular tasks

--- steps/07_deploy_task_dag.py
+++ steps/07_deploy_task_dag.py
@@ -30,8 +30,8 @@ def main(session: Session) -> str:
     dag_name = "HOL_DAG"
     dag = DAG(dag_name, schedule=timedelta(days=1))
     with dag:
-        dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')")
-        dag_task2 = DAGTask("LOAD_DAILY_CITY_METRICS_TASK", definition="CALL LOAD_DAILY_CITY_METRICS_SP()")
+        dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')", warehouse=warehouse_name)
+        dag_task2 = DAGTask("LOAD_DAILY_CITY_METRICS_TASK", definition="CALL LOAD_DAILY_CITY_METRICS_SP()", warehouse=warehouse_name)
 
         dag_task2 >> dag_task1
 
