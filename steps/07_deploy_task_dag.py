443a136be4350031e6e14c2ca94aa3219f492727
Gilberto Hernandez <gilberto.hernandez@snowflake.com>
2024-06-26 23:03:11 -0500

    Revert

--- steps/07_deploy_task_dag.py
+++ steps/07_deploy_task_dag.py
@@ -1,9 +1,9 @@
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Hands-On Lab: Intro to Data Engineering with Snowpark Python
 # Script:       07_deploy_task_dag.py
 # Author:       Jeremiah Hansen
 # Last Updated: 9/26/2023
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 # SNOWFLAKE ADVANTAGE: Snowpark Python API
 # SNOWFLAKE ADVANTAGE: Snowpark Python Task DAG API
@@ -16,17 +16,11 @@ from snowflake.core import Root
 from snowflake.core.task import StoredProcedureCall, Task
 from snowflake.core.task.dagv1 import DAGOperation, DAG, DAGTask
 
-
 # Create the tasks using the DAG API
 def main(session: Session) -> str:
     database_name = "HOL_DB"
     schema_name = "HOL_SCHEMA"
     warehouse_name = "HOL_WH"
-
-    # set database context
-    session.use_database(database_name)
-    # set database schema context
-    session.use_schema(schema_name)
 
     api_root = Root(session)
     schema = api_root.databases[database_name].schemas[schema_name]
@@ -36,16 +30,8 @@ def main(session: Session) -> str:
     dag_name = "HOL_DAG"
     dag = DAG(dag_name, schedule=timedelta(days=1), warehouse=warehouse_name)
     with dag:
-        dag_task1 = DAGTask(
-            "LOAD_ORDER_DETAIL_TASK",
-            definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@EXTERNAL.FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')",
-            warehouse=warehouse_name,
-        )
-        dag_task2 = DAGTask(
-            "LOAD_DAILY_CITY_METRICS_TASK",
-            definition="CALL LOAD_DAILY_CITY_METRICS_SP()",
-            warehouse=warehouse_name,
-        )
+        dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')", warehouse=warehouse_name)
+        dag_task2 = DAGTask("LOAD_DAILY_CITY_METRICS_TASK", definition="CALL LOAD_DAILY_CITY_METRICS_SP()", warehouse=warehouse_name)
 
         dag_task2 >> dag_task1
 
@@ -53,7 +39,7 @@ def main(session: Session) -> str:
     dag_op = DAGOperation(schema)
     dag_op.deploy(dag, mode="orreplace")
 
-    dagiter = dag_op.iter_dags(like="hol_dag%")
+    dagiter = dag_op.iter_dags(like='hol_dag%')
     for dag_name in dagiter:
         print(dag_name)
 
@@ -64,6 +50,6 @@ def main(session: Session) -> str:
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
-if __name__ == "__main__":
+if __name__ == '__main__':
     with Session.builder.getOrCreate() as session:
         main(session)
